90f34faf7e1d3a53d1ba289572cd6348d7f6f808
Eliezer J Romero C <eliezerfot123@gmail.com>
2017-09-13 16:54:53 -0400

    serialize method fields

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -1,4 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+    ModelSerializer, 
+    HyperlinkedIdentityField, 
+    SerializerMethodField
+    )
 
 from posts.models import Post
 
@@ -15,27 +19,60 @@ class PostCreateUpdateSerializer(ModelSerializer):
         ]
 
 
+post_detail_url = HyperlinkedIdentityField(
+    view_name='posts-api:detail',
+    lookup_field='slug'
+)
+
 class PostDetailSerializer(ModelSerializer):
+    url = post_detail_url
+    user = SerializerMethodField()
+    image = SerializerMethodField()
     class Meta:
         model = Post
         fields =[
+            'url',
             'id',
             'title',
             'slug',
             'content',
             'publish',
             'user',
+            'image',
         ]
+    
+    def get_user(self, obj):
+        return str(obj.user.username)
+
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image = None
+        return image
 
 class PostListSerializer(ModelSerializer):
+    url = post_detail_url
+    user = SerializerMethodField()
+    '''    url = HyperlinkedIdentityField(
+        view_name='posts-api:detail',
+        lookup_field='slug'
+    )
+    delete_url = HyperlinkedIdentityField(
+        view_name='posts-api:delete',
+        lookup_field='slug'
+    )'''
     class Meta:
         model = Post
         fields =[
-            'id',
+            'url',
+            'user',
             'title',
             'slug',
             'content',
             'publish',
-            'user',
+            #'delete_url',
         ]
 
+    def get_user(self, obj):
+        return str(obj.user.username)
